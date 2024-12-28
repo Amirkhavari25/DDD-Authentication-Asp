3a66824872d8ce3788380670a867f3d6de6c2f4b
Amir hossein khavari <159920615+Amirkhavari25@users.noreply.github.com>
2024-12-28 17:14:14 +0330

    create context - insert user model from database - install dapper on project sln

---
+++ Infrastructure/Data/DapperDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DapperDbContext : IDisposable
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        private IDbConnection _connection;
+
+        public DapperDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _connectionString = _configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        
+        public IDbConnection CreateConnection()
+        {
+        
+            if (_connection == null || _connection.State != ConnectionState.Open)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            return _connection;
+        }
+
+        
+        public void Dispose()
+        {
+    
+            if (_connection != null && _connection.State == ConnectionState.Open)
+            {
+                _connection.Close(); 
+                _connection.Dispose();
+            }
+        }
+    }
+}
---
+++ Infrastructure/Extensions/DependecyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Extensions
+{
+    public static class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<DapperDbContext>();
+        }
+    }
+}
